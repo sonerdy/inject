8bbb0a95f126069e2dfe0a9cd0abba7fa940deaa
Brandon Joyce <bjoyce@covermymeds.com>
2019-11-14 15:39:14 -0500

    Bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -1,11 +1,11 @@
 defmodule Inject.MixProject do
   use Mix.Project
-  @version "0.1.0"
+  @version "0.2.0"
 
   def project do
     [
       app: :inject,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
